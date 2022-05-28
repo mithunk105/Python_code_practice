a7f6f033ef14e1a5efc11464fb578f4f656c0e9a
Mithun Kumar <mithunk105@gmail.com>
2022-05-28 22:16:21 +0530

    Day20 and Day21 Project practice
    Day20 and Day 21 Python project - Snake game solution

---
+++ food.py
@@ -0,0 +1,19 @@
+from turtle import Turtle
+import random
+
+
+class Food(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_wid=.5, stretch_len=.5)
+        self.color("blue")
+        self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
+        randomx = random.randint(-278, 278)
+        randomy = random.randint(-278, 278)
+        self.goto(randomx, randomy)
